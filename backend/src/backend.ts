babb55fa68721e1d37142e042a15c7671c1ea102
hiverkiya <hiverkiya@gmail.com>
2025-07-02 11:48:55 +0530

    IMPORTANT: Added a binding to fix the typescript error

--- backend/src/backend.ts
+++ backend/src/backend.ts
@@ -1,12 +1,22 @@
 import { Hono } from "hono";
-
-const app = new Hono();
+import { PrismaClient } from "@prisma/client/extension";
+import { withAccelerate } from "@prisma/extension-accelerate";
+// To avoid the type error for the DATABASE_URL, passed a Generic and mentioned the type
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/signup", (c) => {});
+app.post("/api/v1/signup", (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate);
+});
 
 app.post("/api/v1/signin", (c) => {});
 
